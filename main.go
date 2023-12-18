dadbcac82f0001806b5f71dc8a0a4f2266e974a3
WheatleyHDD <wheatleyhdd@gmail.com>
2023-12-18 18:13:05 +0500

    CORS fix

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	"course-tracker/routers"
 )
@@ -10,6 +11,14 @@ func main() {
 	app := fiber.New()
 
 	ConnectDB()
+
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
+
 	routers.Route(app, Db)
 	defer Db.Close()
 
